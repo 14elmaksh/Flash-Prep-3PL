6c0760feb4fe36d00ff75ebacd917f14a7a354ae
Yassin Alisheikh <saelmakki@yahoo.com>
2023-03-02 16:32:41 -0500

    whitelisting server url

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["http://localhost:3000", "https://flash-prep-center.herokuapp.com"];
 
 connectDB();
 
